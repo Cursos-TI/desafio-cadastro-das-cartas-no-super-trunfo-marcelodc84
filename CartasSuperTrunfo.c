575ed7308999cd41799f712cbae1088f78edde2a
marcelodc84 <mdc.marcelo@gmail.com>
2025-05-14 16:53:58 +0000

    Inicio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,17 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+
+// objeto para armazenar os dados da carta
+typedef struct {
+    char codigo[5];
+    char estado;
+    char cidade[50];
+    int populacao;
+    int pontosTuristicos;
+    float area;
+    float pib;
+} CartaCidade;
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
@@ -13,10 +23,11 @@ int main() {
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     return 0;
 }
+
