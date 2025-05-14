ed5e8f638874bb788afa982333fb0a117cb798c0
marcelodc84 <mdc.marcelo@gmail.com>
2025-05-14 16:55:43 +0000

    atribuir dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -20,9 +20,54 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
+    //declaração de variáveis
+    CartaCidade carta1;
+    CartaCidade carta2;
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+
+    //atribuir variáveis
+    printf("Vamos agora cadastrar a carta 1:\n");
+
+    printf("Digite o código da cidade: ");
+    scanf("%s", carta1.codigo);
+    printf("Digite o estado da cidade: ");
+    scanf(" %c", &carta1.estado);
+    printf("Digite o nome da cidade: ");
+    scanf("%s", carta1.cidade);
+    printf("Digite a população da cidade: ");
+    scanf("%d", &carta1.populacao);
+    printf("Digite a área da cidade: ");
+    scanf("%f", &carta1.area);
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &carta1.pib);
+    printf("Digite o número de pontos turísticos da cidade: ");
+    scanf("%d", &carta1.pontosTuristicos);
+
+    printf("=============================\n");
+    printf("Vamos agora cadastrar a carta 2:\n");
+
+    printf("Digite o código da cidade: ");
+    scanf("%s", carta2.codigo);
+    printf("Digite o estado da cidade: ");
+    scanf(" %c", &carta2.estado);
+    printf("Digite o nome da cidade: ");
+    scanf("%s", carta2.cidade);
+    printf("Digite a população da cidade: ");
+    scanf("%d", &carta2.populacao);
+    printf("Digite a área da cidade: ");
+    scanf("%f", &carta2.area);
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &carta2.pib);
+    printf("Digite o número de pontos turísticos da cidade: ");
+    scanf("%d", &carta2.pontosTuristicos);
+
+    printf("=============================\n");
+    printf("Cadastro realizado com sucesso!\n");
+    printf("=============================\n");
+
 
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
